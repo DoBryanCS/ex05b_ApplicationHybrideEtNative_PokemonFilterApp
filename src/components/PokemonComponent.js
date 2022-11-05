5f43bb43534a048ccdef0040cd9a9fc3dfcb6ad3
DoBryanCS <Do.Bryan.CS@gmail.com>
2022-11-05 21:23:22 +0100

    Changement de l'affichage des poketypes

--- src/components/PokemonComponent.js
+++ src/components/PokemonComponent.js
@@ -6,6 +6,14 @@ function PokemonComponent(props) {
   return (
     <div id="pokemons" className="row columns is-multiline is-mobile">
     {props.filteredHabitat.map((pokemon) => {
+        let poke = ''
+        for (let index = 0; index < pokemon.poketypes.length; index++) {
+            if (poke.length === 0) {
+              poke += `${pokemon.poketypes[index].name}`
+            } else {
+                poke += `/${pokemon.poketypes[index].name}`
+            }
+          }
         return (
             <div className="column is-2-desktop is-4-tablet is-full-mobile">
                     <div className="card large" style={{backgroundColor:pokemon.color}}>
@@ -22,7 +30,7 @@ function PokemonComponent(props) {
                                     <p className="title is-4 is-spaced no-padding has-text-centered">{pokemon.name}</p>
                                     <p className="subtitle is-7"><b><span style={{color:'black'}}>Species : </span></b><span style={{color:'black'}}>{pokemon.species.name}</span></p>
                                     <p className="subtitle is-7"><b><span style={{color:'black'}}>Habitat : </span></b><span style={{color:'black'}}>{pokemon.habitat.name}</span></p>
-                                    <p className="subtitle is-7"><b><span style={{color:'black'}}>Poketypes : </span></b>{pokemon.poketypes.map(poketype => (<span style={{color:'black'}}>{poketype.name}/</span>))}</p>
+                                    <p className="subtitle is-7"><b><span style={{color:'black'}}>Poketypes : </span></b><span style={{color:'black'}}>{poke}</span></p>
                                 </div>
                             </div>
                         </div>
